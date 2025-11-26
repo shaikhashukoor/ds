3ef875744761483285790d0b103404f5a70494c5
shaikhashukoor <kkshaikha94@gmail.com>
2025-11-26 11:59:34 +0530

    kruskals.c

--- kruskals.c
+++ kruskals.c
@@ -1,44 +1,59 @@
 #include<stdio.h>
-#include<stdlib.h>
-#define SIZE 10
 
-void main (){
-int u,v,n,i,j,ne=1;
-int visited[10]={0},min,mincost=0,cost[10][10];
-
-printf("\n enter the number of nodes:");
-scanf("%d",&n);
-
-printf("\n enter the adjacent matrix: \n");
-for (i=1;i<=n;i++)
-for (j=1;j<=n;j++)
-{ scanf("%d",&cost[i][j]);
-if(cost[i][j]==0)
-cost[i][j]=999;
-}
-
-visited[1]=1;
-printf("\n");
-
-while(ne<n)
+//minimum spanning tree - kruskal algorithm
+int i,j,u,v,r,ru,rv,n,ne=1;
+int min,mincost=0,cost[9][9],parent[9];
+//find the set reference
+int find(int u)
 {
-min=999;
-
-for(i=1;i<=n;i++)
-for(j=1;j<=n;j++)
-if (cost[i][j] < min)
-if (visited[i] != 0 && visited[j] == 0)
+ while(parent[u])
+ 	u=parent[u];
+ return u;
+}
+//union of two sets
+void uni(int ru,int rv)
 {
-min = cost[i][j];
-u = i;
-v = j;
+ parent[rv]=ru;
+ return;
 }
-printf("\n edge %d : (%d %d) cost : %d",ne++,u,v,min);
-mincost += min;
-visited[v] = 1;
-cost[u][v] = cost[v][u] = 999;
+void main()
+{
+ printf("\nEnter the no.of vertices:");
+ scanf("%d",&n);
+ printf("\nEnter the cost adjacency matrix:\n");
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+  {
+   scanf("%d",&cost[i][j]);
+   if(cost[i][j]==0)
+   cost[i][j]=999; //if no edge,its weight is infinte
+  }
+ }
+ printf("The edges of minimum cost spanning tree are \n");
+ while(ne<n)
+ {
+  min=999;
+  for(i=1;i<=n;i++) //find the minimum cost edge
+  for(j=1;j<=n;j++)
+  if(cost[i][j]<min)
+  {
+   min=cost[i][j];
+   u=i;
+   v=j;
+  }
+  ru=find(u);
+  rv=find(v);
+  if(ru!=rv)
+  {
+   uni(ru,rv);
+   printf("%d edge(%d %d)=%d\n",ne++,u,v,min);
+   mincost+=min;
+  }
+  cost[u][v]=cost[v][u]=999;
+ }
+ printf("\n \t minimum cost=%d\n",mincost);
 }
 
-printf("\n minimum cost = %d",mincost);
-}
+
 
